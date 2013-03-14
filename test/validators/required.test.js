7f910fc775dbcd0f7bc1e95abb7bb56d98d207cc
Adam Duncan <adam.duncan@clock.co.uk>
2013-03-14 18:01:01 +0000

    Fixed required not having runnable tests and fixed an empty object returning true

--- test/validators/required.test.js
+++ test/validators/required.test.js
@@ -1,36 +1,49 @@
-var
-	validators = require('../..').validators,
-	assert = require('assert');
+var isRequired = require('../../validators/required');
 
-module.exports = {
-	'null returns false': function() {
-		assert.strictEqual(validators.isRequired(null), false);
-	},
-	'undefined returns false': function() {
-		assert.strictEqual(validators.isRequired(), false);
-	},
-	'empty string returns false': function() {
-		assert.strictEqual(validators.isRequired(''), false);
-	},
-	'true returns true': function() {
-		assert.strictEqual(validators.isRequired(true), true);
-	},
-	'string returns true': function() {
-		assert.strictEqual(validators.isRequired('abc'), true);
-	},
-	'number returns true': function() {
-		assert.strictEqual(validators.isRequired(123), true);
-	},
-	'populated array returns true': function() {
-		assert.strictEqual(validators.isRequired([1,2,3]), true);
-	},
-	'empty array returns false': function() {
-		assert.strictEqual(validators.isRequired([]), false);
-	},
-	'populated object returns true': function() {
-		assert.strictEqual(validators.isRequired({ foo: 'bar' }), true);
-	},
-	'empty object returns false': function() {
-		assert.strictEqual(validators.isRequired({}), false);
-	}
-};
+describe('Required Validation', function() {
+
+	it('should return false for null value', function() {
+		isRequired(null).should.equal(false)
+	});
+
+	it('should return false for undefined value', function() {
+		isRequired(undefined).should.equal(false)
+	});
+
+	it('should return false for empty string', function() {
+		isRequired('').should.equal(false)
+	});
+
+	it('should return true for true', function() {
+		isRequired(true).should.equal(true)
+	});
+
+	it('should return true for non empty string', function() {
+		isRequired('abc').should.equal(true)
+	});
+
+	it('should return true for number', function() {
+		isRequired(123).should.equal(true)
+	});
+
+	it('should return true for non empty array', function() {
+		isRequired([1, 2, 3]).should.equal(true)
+	});
+
+	it('should return false for empty array', function() {
+		isRequired([]).should.equal(false)
+	});
+
+	it('should return true for non empty object', function() {
+		isRequired({foo: 'bar'}).should.equal(true)
+	});
+
+	it('should return false for empty object', function() {
+		isRequired({}).should.equal(false)
+	});
+
+	it('should return true for date', function() {
+		isRequired(new Date()).should.equal(true)
+	});
+
+});
